9b45e54e04a804be8afb17a6ac476ae801186daa
Casandra <casandragrullon@gmail.com>
2020-07-09 13:42:13 -0400

    attempted longest univalue tree value

--- longest-univalue-treepath.playground/Contents.swift
+++ longest-univalue-treepath.playground/Contents.swift
@@ -28,21 +28,78 @@ class ListNode {
     }
 }
 
-class Solution {
-    func longestUnivaluePath(_ root: TreeNode?) -> Int {
-        //1. check if root is empty
-        guard let _ = root else { return -100}
-        let root = root
-        
-        //2. check if root has left & right branches
-        guard root?.right != nil && root?.left != nil else {
-            return 0
+
+func longestUnivaluePath(_ root: TreeNode?) -> Int {
+    //1. check if root is empty
+    guard let root = root else { return 0}
+    
+    var leftCount = [Int]()
+    var rightCount = [Int]()
+    
+    if let left = root.left {
+        if root.val == left.val {
+            leftCount = checkChild(left, value: left.val)
+            leftCount.append(root.val)
+        } else {
+            leftCount = checkChild(left, value: left.val)
         }
-        
-        while var right = root?.right {
-            
+    }
+    if let right = root.right {
+        if root.val == right.val {
+        rightCount = checkChild(right, value: right.val)
+        rightCount.append(root.val)
+        } else {
+            rightCount = checkChild(right, value: right.val)
         }
-        
-        
+    }
+    
+    if leftCount.count > rightCount.count {
+        return leftCount.count
+    } else {
+        return rightCount.count
     }
 }
+func checkChild(_ root: TreeNode, value: Int) -> [Int] {
+    guard root.val == value else {
+        return []
+    }
+    
+    var leftCount = [Int]()
+    var rightCount = [Int]()
+    
+    if let left = root.left {
+        //leftCount.append(root.val)
+        if left.val == value {
+            leftCount.append(left.val)
+        } else {
+            leftCount.removeAll()
+        }
+    }
+    if let right = root.right {
+        //rightCount.append(root.val)
+        if right.val == value {
+            rightCount.append(right.val)
+        } else {
+            rightCount.removeAll()
+        }
+    }
+    if leftCount.count > rightCount.count {
+        return leftCount
+    } else {
+        return rightCount
+    }
+}
+
+let root = TreeNode(5)
+let left = TreeNode(4)
+let right = TreeNode(5)
+let leftLeft = TreeNode(1)
+let rightLeft = TreeNode(5)
+root.left = left
+root.right = right
+left.left = leftLeft
+right.left = rightLeft
+longestUnivaluePath(root)
+let root2 = TreeNode(1)
+root2.left = TreeNode(1)
+longestUnivaluePath(root2)
