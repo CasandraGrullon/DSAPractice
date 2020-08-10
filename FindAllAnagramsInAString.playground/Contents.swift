621101a4897b11ad824626d2477cc0d4cd98c983
Casandra <casandragrullon@gmail.com>
2020-08-10 12:24:32 -0400

    solved 34/36 test cases

--- FindAllAnagramsInAString.playground/Contents.swift
+++ FindAllAnagramsInAString.playground/Contents.swift
@@ -24,17 +24,20 @@ func findAnagrams(_ s: String, _ p: String) -> [Int] {
     guard s.count > p.count else {
         return []
     }
-    
+
     let sArray = makeArray(s: s, count: p.count)
     let pArray = p.map {$0}
-    let setP = Set(pArray)
     var results = [Int]()
     
     for (index, string) in sArray.enumerated() {
-        let set = Set(string)
-        if set == setP {
+        //this solution solved 34/36 cases
+        if string.sorted() == p.sorted() {
             results.append(index)
         }
+//solution with a set worked for 33/36 test cases
+//        if set == setP {
+//            results.append(index)
+//        }
     }
     
     return results
@@ -60,3 +63,4 @@ func makeArray(s: String, count: Int) -> [String] {
 findAnagrams("abab", "ab") //0,1,2
 findAnagrams("cbaebabacd", "abc") //0,6
 findAnagrams("baa", "aa") //1
+findAnagrams("ababababab", "aab") //0,2,4,6
