3299c5d61c60e75df98edbe3ab1a91663c4d40a8
Casandra <casandragrullon@gmail.com>
2020-08-31 17:06:19 -0400

    solved for some test cases

--- Arrays/MaxProductofTwoElements.playground/Contents.swift
+++ Arrays/MaxProductofTwoElements.playground/Contents.swift
@@ -44,51 +44,66 @@ maxProduct([7,3])//12
 //Explanation: The 8 subarrays that have product less than 100 are: [10], [5], [2], [6], [10, 5], [5, 2], [2, 6], [5, 2, 6].
 //Note that [10, 5, 2] is not included as the product of 100 is not strictly less than k.
 func numSubarrayProductLessThanK(_ nums: [Int], _ k: Int) -> Int {
-    
     guard !nums.isEmpty else {
         return 0
     }
-    var count = 0
-    for num in nums {
-        if num < k {
-            count += 1
+    var set: Set<[Int]> = []
+    
+    for num in 0..<nums.count {
+        if nums[num] < k {
+            set.insert([num])
         }
     }
+    
+    var index = 0
+    
+    while index < nums.count {
+        if let subArray = createSubArrays(array: nums, start: index, end: nums.count - 1, k: k) {
+            set.insert(subArray)
+        }
+        index += 1
+    }
+    
     let middleIndex = nums.count / 2
-    var lowSub = Array(nums[..<middleIndex])
-    var upSub = Array(nums[middleIndex...])
-    
-    count += checkSubArray(array: nums, k: k)
-    //count += checkSubArray(array: lowSub, k: k)
-    //count += checkSubArray(array: upSub, k: k)
-    
-    while upSub.count > 0 {
-        count += checkSubArray(array: lowSub, k: k)
-        print(lowSub)
-        if let first = upSub.first {
-            lowSub.append(first)
-            upSub.removeFirst()
+    for i in 0...middleIndex {
+        if let subArray = createSubArrays(array: nums, start: i, end: middleIndex, k: k) {
+            var indicies = [Int]()
+            indicies.append(contentsOf: subArray.indices)
+            set.insert(indicies)
         }
     }
-    lowSub = Array(nums[..<middleIndex])
-    upSub = Array(nums[middleIndex...])
-    while lowSub.count > 0 {
-        count += checkSubArray(array: upSub, k: k)
-        print(upSub)
-        if let last = lowSub.last {
-            upSub.append(last)
-            lowSub.removeLast()
+    for j in middleIndex..<nums.count {
+        if let subArray = createSubArrays(array: nums, start: j, end: nums.count - 1, k: k) {
+            var indicies = [Int]()
+            indicies.append(contentsOf: subArray.indices)
+            set.insert(indicies)
         }
     }
-    
-    return count
+    print(set)
+    return set.count
 }
-func checkSubArray(array: [Int], k: Int) -> Int {
-    var count = 0
+func checkSubArray(array: [Int], k: Int) -> Bool {
     if array.reduce(1, *) < k {
-        count += 1
+        return true
     }
-    return count
+    return false
 }
-numSubarrayProductLessThanK([10,5,2,6], 100)
-numSubarrayProductLessThanK([100], 100)
+func createSubArrays(array: [Int], start: Int, end: Int, k: Int) -> [Int]? {
+    var newArray = Array(array[start...end])
+    
+    guard newArray.count > 1 else {
+        return nil
+    }
+    while newArray.count > 1 {
+        if checkSubArray(array: newArray, k: k) {
+            return newArray
+        }
+        newArray.removeLast()
+    }
+    
+    return nil
+}
+numSubarrayProductLessThanK([10,5,2,6], 100)//8
+numSubarrayProductLessThanK([100], 100)//0
+numSubarrayProductLessThanK([1,1,1], 2)//6
+numSubarrayProductLessThanK([10,9,10,4,3,8,3,3,6,2,10,10,9,3], 19)//18
